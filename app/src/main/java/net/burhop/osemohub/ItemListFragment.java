61f21564a0828a4380dacd20ac044b376a0ff1a3
burhma00 <mark.burhop@siemens.com>
2014-10-12 18:06:43 -0400

    just a test to be sure I can checkin

--- app/src/main/java/net/burhop/osemohub/ItemListFragment.java
+++ app/src/main/java/net/burhop/osemohub/ItemListFragment.java
@@ -97,6 +97,7 @@ public class ItemListFragment extends ListFragment {
         // Activities containing this fragment must implement its callbacks.
         if (!(activity instanceof Callbacks)) {
             throw new IllegalStateException("Activity must implement fragment's callbacks.");
+            
         }
 
         mCallbacks = (Callbacks) activity;
